737143fd22ecc171dae70425c68e91a2a70b0812
Jobayerdev <jobayerhossain977@gmail.com>
2022-01-21 00:58:12 +0600

    08-Separate-JSX-to-Component: Added

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -6,6 +6,9 @@ const HomePage = () => {
             <a href="/">Home</a>
             <a href="/about">About</a>
             <h1>This is Home Page </h1>
+            <button onClick={ () => {
+                console.log('I am Called')
+            } }>Press me!</button>
         </main>
     )
 }
