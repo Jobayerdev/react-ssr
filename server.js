b5dadc9307cb9f8aebaf782e51403fa80386ad3d
Jobayerdev <jobayerhossain977@gmail.com>
2022-01-20 20:58:48 +0600

    04.-Very-First-SSR-App: Added

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const app = express()
 
+let message = 'Hello World'
+
+app.get('/', (req, res) => {
+    res.send(`<h1>${message}</h1>`)
+})
+
 app.listen(3000, () => {
     console.log('Listing on PORT 3000')
 })
