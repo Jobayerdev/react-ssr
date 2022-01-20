13bece58af603d57f10b8e4cb33a125585edccb0
Jobayerdev <jobayerhossain977@gmail.com>
2022-01-21 00:11:59 +0600

    05.-React-on-Server: Added

--- server.js
+++ server.js
@@ -4,49 +4,30 @@ const ReactDOM = require('react-dom/server')
 const React = require('react')
 
 app.get('/', (req, res) => {
-    res.send(`
-    <main>
-        <nav>
-            <ul>
-                <li><a href="/">Home</a></li>
-                <li><a href="/about">About</a></li>
-                <li><a href="/contact">Contact</a></li>
-            </ul>
-        </nav>
-       <h1>This is Home page </h1>
-    </main>
-    `)
-})
-
-app.get('/contact', (req, res) => {
-    const withoutJsx = React.createElement(
-        'div',
-        null,
-        React.createElement("h1", null, `This is Contact page`),
-        React.createElement("h2", null, `Phone: +8801923111111`),
-        React.createElement('p', null, '6 North Kafrul')
-    )
-    const jsx = `
-        <div>
-            <h1> Phone: +8801923111111 </h1>
-            <p> 6 North Kafrul </p>
-        </div>
+    const html = `
+        <main>
+            <a href="/">Home</a>
+            <a href="/about">About</a>
+            <h1>This is Home page </h1>
+        </main>
     `
+    const withoutJsx = React.createElement('main',
+        null,
+        React.createElement('a', { href: '/' }, 'Home'),
+        React.createElement('a', { href: '/about' }, 'About'),
+        React.createElement('h1', null, 'This is Home page')
+    )
+
     res.send(ReactDOM.renderToString(withoutJsx))
 })
 
 app.get('/about', (req, res) => {
     res.send(`
-    <main>
-        <nav>
-            <ul>
-                <li><a href="/">Home</a></li>
-                <li><a href="/about">About</a></li>
-                <li><a href="/contact">Contact</a></li>
-            </ul>
-        </nav>
-        <h1>This is About page </h1>
-    </main>
+        <main>
+            <a href="/">Home</a>
+            <a href="/about">About</a>
+            <h1>This is About page </h1>
+        </main>
     `)
 })
 
